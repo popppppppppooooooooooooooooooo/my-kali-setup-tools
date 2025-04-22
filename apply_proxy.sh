c7ac2b608c50db7829a8f7f80515e0b1d7851152
popppppppppooooooooooooooooooo <157764817+popppppppppooooooooooooooooooo@users.noreply.github.com>
2025-04-22 11:53:48 +0900

    Update apply_proxy.sh
    handle error

--- apply_proxy.sh
+++ apply_proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo su
 
 echo "enter the http proxy server address"
 read host
@@ -6,13 +7,14 @@ read host
 echo "enter the http proxy server port"
 read port
 
+readonly pass
 readonly host
 readonly port
 
-sudo su
 
 # find *.h >> command.log 2>&1
 
+set -e
 
 echo 'Acquire::http::Proxy "http://'${host}':'${port}'";\nAcquire::https::Proxy "http://'${host}':'${port}'";' | tee -a /etc/apt/apt.conf
 echo 'https_proxy = '${host}':'${port}\\n'http_proxy = '${host}':'${port} | tee -a /etc/wget
@@ -20,16 +22,18 @@ echo 'proxy=http://'${host}':'${port} | tee -a ~/.curlrc
 echo 'export https_proxy="http://'${host}':'${port}'/"\nexport http_proxy="http://'${host}':'${port}'/"\nexport HTTPS_PROXY="http://'${host}':'${port}'/"\nexport HTTP_PROXY="http://'${host}':'${port}'/"' | tee -a ~/.zshrc
 echo 'export https_proxy="http://'${host}':'${port}'/"\nexport http_proxy="http://'${host}':'${port}'/"\nexport HTTPS_PROXY="http://'${host}':'${port}'/"\nexport HTTP_PROXY="http://'${host}':'${port}'/"' | tee -a ~/.bashrc
 
-
 mkdir /etc/systemd/system/docker.service.d
 echo '[Service]\nEnvironment=HTTP_PROXY="http://'${host}':'${port}'/"\nEnvironment=HTTPS_PROXY="http://'${host}':'${port}'/"' | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf
 
 mkdir ~/.docker
 echo '{\n  "proxies": {\n    "default": {\n      "httpProxy": "http://'${host}':'${port}'/",\n      "httpsProxy": "http://'${host}':'${port}'/",\n      "noProxy": "127.0.0.0/8"\n    }\n  }\n}' | tee -a ~/.docker/config.json
 
+echo 'http_proxy=http://'${host}':'${port}'/\nhttps_proxy=http://'${host}':'${port}'/' | tee -a /etc/wgetrc
+
+set +e
+
 git config --global http.proxy http://${host}:${port}
 
+exit
 
-
-
-echo "finished"
+echo "\n=====================\n      finished\n=====================\n"
